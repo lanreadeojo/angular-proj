c695161614c992946d243003e67588112e5e29ac
Lanre Adeojo <lanreadeojo03@gmail.com>
2023-09-04 22:03:54 -0400

    add changeDetection to nodes-row and add event emitter to navber

--- datacenter/src/app/metric/metric.component.ts
+++ datacenter/src/app/metric/metric.component.ts
@@ -1,28 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input,ChangeDetectionStrategy, OnChanges} from '@angular/core';
 
 @Component({
   selector: 'app-metric',
   templateUrl: './metric.component.html',
-  styleUrls: ['./metric.component.css']
-})
-export class MetricComponent {
+  styleUrls: ['./metric.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
   
-private _value: number= 0
-private _max: number= 100
-
-@Input('used') 
-set value(value: number){
-  if(isNaN(value)){value=0}
-  this._value = value
+})
+export class MetricComponent implements OnChanges{
+  
+@Input('used') value: number = 0;
+@Input('available') max: number =100;
+ngOnChanges(changes: any){
+  if(changes.value && isNaN(changes.value.currentValue)){this.value=0;}
+  if(changes.max && isNaN(changes.max.currentValue)){this.value=0;}
 }
-get value():number {return this._value}
-
-@Input('available')
-set max(max: number){
-  if(isNaN(max)){max=0}
-  this._max=max;
-}
-get max():number {return this._max}
 isDanger(){
   return this.value / this.max > 0.7
 }
